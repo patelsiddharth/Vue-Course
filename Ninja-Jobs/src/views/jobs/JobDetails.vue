d575ac3e41807b5c2b81ae3f19be75a83d746a0b
Siddharth Patel <siddharthpatel425@gmail.com>
2021-07-09 10:54:41 +0530

    Add files via upload

---
+++ Ninja-Jobs/src/views/jobs/JobDetails.vue
@@ -0,0 +1,33 @@
+<template>
+    <div v-if="job">
+        <h1>{{job.title}}</h1>
+        <p>Job ID is {{$route.params.id}}</p>
+        <p>{{job.details}}</p>
+    </div>
+    <div v-else>
+        <p>Loading job details...</p>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['id'],
+    data()
+    {
+        return {
+            job : null
+        }
+    },
+    mounted()
+    {
+        fetch('http://localhost:3000/jobs/' + this.id)
+        .then(res => res.json())
+        .then(data => this.job = data)
+        .catch(error => console.log(error))
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ Ninja-Jobs/src/views/jobs/Jobs.vue
@@ -0,0 +1,54 @@
+<template>
+<div>
+    <div v-if="jobs.length">
+        <div class="job" v-for="job in jobs" :key="job.id">
+            <router-link :to="{name: 'JobDetails', params: {id : job.id}}">
+                <h2>{{job.title}}</h2>
+            </router-link>
+        </div>
+    </div>
+    <div v-else>
+        <p>Loading jobs...</p>
+    </div>
+</div>
+</template>
+
+<script>
+export default 
+{
+    data()
+    {
+        return {
+            jobs : []
+        }
+    },
+    mounted()
+    {
+        fetch('http://localhost:3000/jobs')
+        .then(res => res.json())
+        .then(data => this.jobs = data)
+        .catch(error => console.log(error))
+    }
+}
+</script>
+
+<style>
+.job h2
+{
+    background: #f4f4f4;
+    padding: 20px;
+    border-radius: 10px;
+    margin: 10px auto;
+    max-width: 600px;
+    cursor: pointer;
+    color: #444;
+}
+.job h2:hover
+{
+    background: #ddd;
+}
+.job a
+{
+    text-decoration: none;
+}
+</style>
