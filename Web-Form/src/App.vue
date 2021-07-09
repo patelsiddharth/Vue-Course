2caf1d0c1f2febf97a018a7b3ddf98d504e0b8f5
Siddharth Patel <siddharthpatel425@gmail.com>
2021-07-09 10:59:10 +0530

    Add files via upload

---
+++ Web-Form/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <SignUpForm />
+    </div>
+</template>
+
+<script>
+import SignUpForm from './components/signUpForm';
+
+export default {
+    name: 'App',
+    components: {
+        SignUpForm
+    }
+}
+</script>
+
+<style>
+body, #app
+{
+    font-family:Arial, Helvetica, sans-serif;
+    margin: 0;
+    background: #eee;
+}
+</style>
